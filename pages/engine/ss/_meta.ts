e96962b799f933c7872e4459490e04e982e47d03
isoubelet <isoubelet@bluelone.com>
2025-11-05 18:46:40 +0100

    Adjusted to cover v25.6

--- pages/engine/ss/_meta.ts
+++ pages/engine/ss/_meta.ts
@@ -2,5 +2,6 @@ export default {
   "sys": "SYS Schema",
   "sysadmin": "SYSADMIN Schema",
   "sysmetrics": "SYSMETRICS Schema",
-  "sysregistry": "SYSREGISTRY Schema"
+  "sysregistry": "SYSREGISTRY Schema",
+  "stx": "SOFT_TRANSACTIONS Schema"
 }
--- pages/observability/_meta.ts
+++ pages/observability/_meta.ts
@@ -1,3 +1,6 @@
 export default {
-  "metrics": ""
+  "intro": "Introduction",
+  "logs": "",
+  "metrics": "",
+  "tracing": ""
 }
